5fe83a3975cc77567e334adf1a805fcded08c60e
Eric Karge <eric.karge@hypoport.de>
2014-07-28 19:06:53 +0200

    working filtering

--- js/polyfill.js
+++ js/polyfill.js
@@ -28,7 +28,7 @@ if (!String.prototype.trim) {
 }
 
 function textContent(element) {
-  return (element.textContent || element.innerText);
+  return element.textContent || (element.textContent = element.innerText);
 }
 
 var classListAdd;
@@ -39,11 +39,11 @@ if (document.documentElement.classList) {
     return element.classList.contains(value);
   };
 
-  classListRemove = function (choice, s) {
+  classListRemove = function (element, value) {
     return element.classList.remove(value);
   };
 
-  classListAdd = function (choice, s) {
+  classListAdd = function (element, value) {
     return element.classList.add(value);
   };
 }
@@ -71,3 +71,66 @@ function toArray(arrayLike) {
   return result;
 }
 
+var nextElementSibling;
+if (typeof document.documentElement.nextElementSibling != "undefined") {
+  nextElementSibling = function (element) {
+    return element.nextElementSibling;
+  };
+} else {
+  nextElementSibling = function (element) {
+    for (var sibling = element.nextSibling; sibling; sibling = sibling.nextSibling) {
+      if (sibling instanceof Element) {
+        break;
+      }
+    }
+    return sibling;
+  };
+}
+
+var previousElementSibling;
+if (typeof document.documentElement.previousElementSibling != "undefined") {
+  previousElementSibling = function (element) {
+    return element.previousElementSibling;
+  };
+} else {
+  previousElementSibling = function (element) {
+    for (var sibling = element.nextSibling; sibling; sibling = sibling.previousSibling) {
+      if (sibling instanceof Element) {
+        break;
+      }
+    }
+    return sibling;
+  };
+}
+
+var firstElementChild;
+if (typeof document.documentElement.firstElementChild != "undefined") {
+  firstElementChild = function (element) {
+    return element.firstElementChild;
+  };
+} else {
+  firstElementChild = function (element) {
+    for (var child = element.firstChild; child; child = child.nextSibling) {
+      if (child instanceof Element) {
+        break;
+      }
+    }
+    return child;
+  };
+}
+
+var lastElementChild;
+if (typeof document.documentElement.lastElementChild != "undefined") {
+  lastElementChild = function (element) {
+    return element.lastElementChild;
+  };
+} else {
+  lastElementChild = function (element) {
+    for (var child = element.lastChild; child; child = child.previousSibling) {
+      if (child instanceof Element) {
+        break;
+      }
+    }
+    return child;
+  };
+}
